367ad9d05e4301b09871c0ed9d787d3565cc75f9
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-02-28 22:10:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name='user_models',
-    version='1.1.0',
+    version='1.1.1',
     packages=setuptools.find_packages(),
     # scripts=['makemigrations.py','migrate.py']
 )
