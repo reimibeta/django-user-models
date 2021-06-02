56e2aca26aec909de180bd366196728841a741d3
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-02 09:39:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ import setuptools
 
 setuptools.setup(
     name='user_models',
-    version='1.2.1',
+    version='1.2.2',
     packages=setuptools.find_packages(),
-    install_requires=[
-        'image-utils @ git+https://github.com/reimibeta/django-image-utils.git',
-        'datetime-utils @ git+https://github.com/reimibeta/django-datetime-utils.git',
-        'html-render-utils @ git+https://github.com/reimibeta/django-html-render-utils.git',
-        'djangorestframework==3.12.4',
-        'djangorestframework-simplejwt==4.7.0',
-        'drf-flex-fields==0.9.0',
-        'django-admin-list-filter-dropdown==1.0.3',
-        'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
-    ]
+    # install_requires=[
+    #     'image-utils @ git+https://github.com/reimibeta/django-image-utils.git',
+    #     'datetime-utils @ git+https://github.com/reimibeta/django-datetime-utils.git',
+    #     'html-render-utils @ git+https://github.com/reimibeta/django-html-render-utils.git',
+    #     'djangorestframework==3.12.4',
+    #     'djangorestframework-simplejwt==4.7.0',
+    #     'drf-flex-fields==0.9.0',
+    #     'django-admin-list-filter-dropdown==1.0.3',
+    #     'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
+    # ]
 
     # scripts=['makemigrations.py','migrate.py']
 )
